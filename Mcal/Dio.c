27665b76cbc8533c94419984f05ef2f38c6064ff
merna7ossam <mernahossam87@gmail.com>
2021-01-15 17:52:12 +0200

    add dio write

--- Mcal/Dio.c
+++ Mcal/Dio.c
@@ -17,12 +17,69 @@ void Dio_Init(void)
 }
 void Dio_WriteChannel(Dio_ChannelType ChannelId, Dio_ChannelLevelType ChannelLevel)
 {
-     Dio_PortType PortNum = ChannelId/NUM_OF_CHANNELS_IN_PORT;
-     uint8 BitOffset = ChannelId%NUM_OF_CHANNELS_IN_PORT;
+	Dio_PortType PortNum = ChannelId/NUM_OF_CHANNELS_IN_PORT;
+	uint8 BitOffset = ChannelId%NUM_OF_CHANNELS_IN_PORT;
+	if(ChannelLevel == STD_HIGH)
+	{
+		switch (PortNum) {
+		case Dio_Port_A:
+			SET_BIT(PORTA,BitOffset);
+			break;
+		case Dio_Port_B:
+			SET_BIT(PORTB,BitOffset);
+			break;
+		case Dio_Port_C:
+			SET_BIT(PORTC,BitOffset);
+			break;
+		case Dio_Port_D:
+			SET_BIT(PORTD,BitOffset);
+			break;
+		default:
+			break;
+		}
 
+	}
+	else
+	{
+
+		switch (PortNum) {
+		case Dio_Port_A:
+			CLR_BIT(PORTA,BitOffset);
+			break;
+		case Dio_Port_B:
+			CLR_BIT(PORTB,BitOffset);
+			break;
+		case Dio_Port_C:
+			CLR_BIT(PORTC,BitOffset);
+			break;
+		case Dio_Port_D:
+			CLR_BIT(PORTD,BitOffset);
+			break;
+		default:
+			break;
+	}
 }
 Dio_ChannelLevelType Dio_ReadChannel(Dio_ChannelType ChannelId)
 {
+	Dio_PortType PortNum = ChannelId/NUM_OF_CHANNELS_IN_PORT;
+	uint8 BitOffset = ChannelId%NUM_OF_CHANNELS_IN_PORT;
+
+
+	switch (PortNum) {
+			case Dio_Port_A:
+				GET_BIT(PORTA,BitOffset);
+				break;
+			case Dio_Port_B:
+				GET_BIT(PORTB,BitOffset);
+				break;
+			case Dio_Port_C:
+				GET_BIT(PORTC,BitOffset);
+				break;
+			case Dio_Port_D:
+				GET_BIT(PORTD,BitOffset);
+				break;
+			default:
+				break;
 
 
 
